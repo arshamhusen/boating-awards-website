ef5dd9190a9302d0a5a88a7f992093a9569cd02d
arhsamhusen <arushamhussain@icloud.com>
2022-06-27 22:08:07 +0500

    Minor Changed

--- src/components/winner-tab-menu/WinnerTabMenu.js
+++ src/components/winner-tab-menu/WinnerTabMenu.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Tab } from "@headlessui/react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const responsive = {
   desktop: {
@@ -20,21 +22,6 @@ const responsive = {
     paritialVisibilityGutter: 30,
   },
 };
-
-const images = [
-  "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550353175-a3611868086b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550330039-a54e15ed9d33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1549737328-8b9f3252b927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1549833284-6a7df91c1f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1549985908-597a09ef0a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-];
 
 const winners = [
   {
@@ -202,11 +189,16 @@ function renderPanel(year) {
 }
 
 function AboutTabMenu() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <div>
       <Tab.Group>
         <div className="flex justify-start md:justify-center  items-center">
           <Tab.List
+            data-aos="fade-up"
             className={`px-10 md:px-20 lg:px-20 grid grid-cols-6 gap-x-2 md:gap-x-8`}
           >
             {winners.map((year) => (
@@ -230,8 +222,10 @@ function AboutTabMenu() {
         <Tab.Panels>
           {winners.map((year) => (
             <>
-              <Tab.Panel className={`mt-5 w-screen`}>
+              <Tab.Panel data-aos="fade-up" className={`mt-5 w-screen`}>
                 <Carousel
+                  autoPlay
+                  infinite
                   ssr
                   partialVisbile
                   itemClass="slider-image-item"
