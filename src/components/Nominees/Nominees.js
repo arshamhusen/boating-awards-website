d0ac45daccedee4abee892b76f33aa6953ab1dd3
arhsamhusen <arushamhussain@icloud.com>
2022-08-01 10:03:59 +0500

    Awards text center

--- src/components/Nominees/Nominees.js
+++ src/components/Nominees/Nominees.js
@@ -214,7 +214,7 @@ function Index() {
       >
         {/* Category Image */}
 
-        <div className=" w-full flex flex-col justify-center items-center space-y-10 p-10">
+        <div className=" w-full flex flex-col justify-center text-center items-center space-y-10 p-10">
           <h1 className="text-3xl w-2/3 text-start lg:text-5xl font-bold  text-secondary leading-tight">
             {pageData.category_name}
           </h1>
